288e51f23ab1c4e656eac005dbfecc1805ac1649
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2026-02-23 08:31:22 -0300

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,43 @@
 <template>
-  <NavBar/>
-  <router-view />
+  <div class="app-layout">
+    <NavBar @status-sidebar="atualizarMargem" />
+    
+    <main :class="['main-content', { 'sidebar-expandida': sidebarAberta }]">
+      <router-view />
+    </main>
+  </div>
 </template>
 
-<script >
+<script>
 import NavBar from './components/NavBar.vue'
 
-  export default {
-  name: 'App',
-  components: {
-    NavBar
+export default {
+  components: { NavBar },
+  data() {
+    return {
+      sidebarAberta: true // Começa igual ao estado inicial da sidebar
+    }
+  },
+  methods: {
+    atualizarMargem(status) {
+      this.sidebarAberta = status;
     }
   }
+}
 </script>
 
 <style>
 body{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+  font-family: var(--font);
+}
+.main-content {
+  transition: margin-left 0.3s ease; /* Suaviza o movimento junto com a sidebar */
+  margin-left: 200px;
+  width: calc(100% -200px);
+  flex: 1;
+}
+
+.main-content.sidebar-expandida {
+  margin-left: 200px; /* Margem quando a sidebar está aberta */
 }
 </style>
-
